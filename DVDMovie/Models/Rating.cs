8641536650b51a1b384e6eb0abe259e84aeed100
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2020-08-18 02:34:50 +0530

    Add files via upload

---
+++ DVDMovie/Models/Rating.cs
@@ -0,0 +1,10 @@
+namespace DVDMovie.Models
+{
+    public class Rating
+    {
+        
+        public long RatingId { get; set; }
+        public int Stars { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
---
+++ DVDMovie/Startup.cs
@@ -0,0 +1,105 @@
+using DVDMovie.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace DVDMovie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+                services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(Configuration
+                    ["Data:Movies:ConnectionString"]));
+
+            services.AddMvc().AddNewtonsoftJson(opts =>{
+                		opts.SerializerSettings.ReferenceLoopHandling
+                   		 	= ReferenceLoopHandling.Serialize;
+                                //null avoid karne k liye
+                        opts.SerializerSettings.NullValueHandling 
+                            = NullValueHandling.Ignore;
+                });
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+             // Added for handling sessions
+            services.AddDistributedSqlServerCache(options =>
+            {
+                options.ConnectionString =
+                    Configuration["Data:Movies:ConnectionString"];
+                options.SchemaName = "dbo";
+                options.TableName = "SessionData";
+            });
+            // Added for handling sessions
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = "DVDMovie.Session";
+                options.IdleTimeout = System.TimeSpan.FromHours(48);
+                options.Cookie.HttpOnly = false;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
